8412ea572aa98318da4be881ced27e2ffde63ade
Spotky1004 <spotky1004@gmail.com>
2021-08-30 11:33:57 +0900

    fix: Fix 3-5

--- src/Worlds/Main/Chapter3.js
+++ src/Worlds/Main/Chapter3.js
@@ -69,8 +69,8 @@ const Chapter = {
             },
             Difficulty: 5,
             Title: "Multiply & Divide",
-            Description: "Make 240",
-            Goal: ["240"],
+            Description: "Make 60",
+            Goal: ["60"],
             Items: [
                 [
                     ["number", { symbol: 7 }],
