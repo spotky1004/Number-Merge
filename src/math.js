2251fdd5c49197a7eb6c239efadc702ed645f9c9
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2021-11-14 21:46:33 +0800

    Update math.js

--- src/math.js
+++ src/math.js
@@ -1,8 +1,8 @@
 export function factorial(from, count = from) {
     //if (Math.abs(count) > 200) return Infinity;
-    if (Math.abs(count) > 200 && !(count > from) && from < 0) return Infinity;
-    if (count > from && from >= 0) return 0;
-    if (Math.abs(count) > 200 && from < 0) return Infinity * (-1) ** count;
+    if (Math.abs(count) > 200 && !(count > from) && from < 0){return Infinity;}
+    if (count > from && from >= 0){return 0;}
+    if (Math.abs(count) > 200 && from < 0){return Infinity * (-1) ** count;}
 
     let res = 1;
     for (let i = from; i > from - Math.abs(count); i--) {
@@ -20,7 +20,7 @@ export function sigma(from, count = from) {
         res += i;
     }*/
     
-    res = Math.abs(count)*from + Math.abs(count)*(Math.abs(count)-1)*-0.5
+    res = Math.abs(count)*from + Math.abs(count)*(Math.abs(count)-1)*-0.5;
     
     return res;
 }
